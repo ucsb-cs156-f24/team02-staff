f56e4128c2a21faea70fa19c475200c3d1906abe
Sangita Kunapuli <sangita@Sangitas-MacBook-Pro.local>
2024-10-30 14:18:37 -0700

    sk - Add fixtures file for Articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,40 @@
+const articlesFixtures = {
+    oneArticle:
+    {
+        "id": 1,
+        "title": "Test Articles",
+        "url": "https://www.google.com",
+        "explanation": "1",
+        "email": "me@ucsb.edu",
+        "dateAdded": "2022-01-03T00:10:00",
+    },
+    threeArticles:
+    [
+        {
+            "id": 2,
+            "title": "Test Articles",
+            "url": "https://www.google.com",
+            "explanation": "1",
+            "email": "me@ucsb.edu",
+            "dateAdded": "2022-01-03T00:10:00"
+        },
+        {
+            "id": 3,
+            "title": "Test Articles",
+            "url": "https://www.images.google.com",
+            "explanation": "1",
+            "email": "me@ucsb.edu",
+            "dateAdded": "2022-01-03T00:10:00"
+        },
+        {
+            "id": 4,
+            "title": "Test Articles",
+            "url": "https://www.google.com",
+            "explanation": "1",
+            "email": "me@ucsb.edu",
+            "dateAdded": "2022-01-03T00:10:00"
+        },
+    ]
+};
+
+export { articlesFixtures };
