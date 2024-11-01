58966dab94f6d48fa53ef10c7ddfd7d2717e4e45
RaySu <hongrui_su@ucsb.edu>
2024-10-31 17:33:02 -0700

    add fixtures file

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,31 @@
+const ucsbOrganizaitonFixtures = {
+    oneOrganizaiton: {
+        orgCode: "ZBT",
+        orgTranslationShort: "ZETA BETA TAU",
+        orgTranslation: "ZETA BETA TAU",
+        inactive: true,
+    },
+    threeOrganizaitons: [
+      {
+        orgCode: "ZBZ",
+        orgTranslationShort: "ZETA BETA ZETA",
+        orgTranslation: "ZETA BETA TAU",
+        inactive: false,
+      },
+      {
+        orgCode: "ZBB",
+        orgTranslationShort: "ZETA BETA BETA",
+        orgTranslation: "ZETA BETA TAU",
+        inactive: false,
+      },
+      {
+        orgCode: "ZTT",
+        orgTranslationShort: "ZETA TAU TAU",
+        orgTranslation: "ZETA BETA TAU",
+        inactive: false,
+      },
+    ],
+  };
+  
+  export { ucsbOrganizaitonFixtures };
+  
