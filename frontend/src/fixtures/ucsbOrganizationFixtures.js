5a81a10e87c37ab9e0f711d01e0b0f83bfa99afe
RaySu <hongrui_su@ucsb.edu>
2024-10-31 18:20:26 -0700

    fix the format

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,31 +1,30 @@
-const ucsbOrganizaitonFixtures = {
-    oneOrganizaiton: {
-        orgCode: "ZBT",
-        orgTranslationShort: "ZETA BETA TAU",
-        orgTranslation: "ZETA BETA TAU",
-        inactive: true,
+const ucsbOrganizationFixtures = {
+  oneOrganization: {
+    orgCode: "ZBT",
+    orgTranslationShort: "ZETA BETA TAU",
+    orgTranslation: "ZETA BETA TAU",
+    inactive: true,
+  },
+  threeOrganizations: [
+    {
+      orgCode: "ZBZ",
+      orgTranslationShort: "ZETA BETA ZETA",
+      orgTranslation: "ZETA BETA TAU",
+      inactive: false,
     },
-    threeOrganizaitons: [
-      {
-        orgCode: "ZBZ",
-        orgTranslationShort: "ZETA BETA ZETA",
-        orgTranslation: "ZETA BETA TAU",
-        inactive: false,
-      },
-      {
-        orgCode: "ZBB",
-        orgTranslationShort: "ZETA BETA BETA",
-        orgTranslation: "ZETA BETA TAU",
-        inactive: false,
-      },
-      {
-        orgCode: "ZTT",
-        orgTranslationShort: "ZETA TAU TAU",
-        orgTranslation: "ZETA BETA TAU",
-        inactive: false,
-      },
-    ],
-  };
-  
-  export { ucsbOrganizaitonFixtures };
-  
+    {
+      orgCode: "ZBB",
+      orgTranslationShort: "ZETA BETA BETA",
+      orgTranslation: "ZETA BETA TAU",
+      inactive: false,
+    },
+    {
+      orgCode: "ZTT",
+      orgTranslationShort: "ZETA TAU TAU",
+      orgTranslation: "ZETA BETA TAU",
+      inactive: false,
+    },
+  ],
+};
+
+export { ucsbOrganizationFixtures };
